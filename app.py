c19baba7fdc20d73a7a7715a4907df58bed0ea5c
Maritrini <a329675@uach.mx>
2024-10-31 19:18:06 -0600

    implemantaciones

--- app.py
+++ app.py
@@ -10,10 +10,21 @@ import joblib
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier, AdaBoostClassifier, GradientBoostingClassifier, BaggingClassifier
+import xgboost as xgb
 
 modelLR = joblib.load('model_LR.sav')
 modelPoly = joblib.load('model_poly.sav')
 modelRBF = joblib.load('model_rbf.sav')
+modelAda = joblib.load('ada_clf2.sav')
+modelBag = joblib.load('bag_clf.sav')
+modelGradBoost = joblib.load('grad_boost_clf.sav')
+modelDT = joblib.load('dt_best_smote_scaled.sav')
+modelRF = joblib.load('rf_best_smote_scaled.sav')
+modelVote = joblib.load('voting_clf.sav')
+modelXGB = joblib.load('xgb_clf2.sav')
+
 #pipeline = joblib.load('stroke_pipeline.sav')
 
 def main():
@@ -28,7 +39,8 @@ def main():
     avg_glucose_level = st.number_input("avg glucose level",min_value=55.12,max_value=271.74,value="min")
     bmi = st.number_input("bmi",min_value=10.30,max_value=97.60,value="min")
     smoking_status = st.selectbox("Smoking Status",["formerly smoked","never smoked","smokes","Unknown"])
-    select_model = st.selectbox("Select the model you want to try",["LogisticRegression","Poly-SVC","RBF-SVC"])
+    select_model = st.selectbox("Select the model you want to try",["LogisticRegression","Poly-SVC","RBF-SVC","AdaBoost","Bagging","GradientBoost",
+                                                                    "DecisionTree","RandomForest","Voting","XGBoost"])
     if st.button("Predict Stroke"):
         features = [[gender,age,hypertension,heart_disease,ever_married,work_type,Residence_type,avg_glucose_level,
                      bmi,smoking_status]]
@@ -53,6 +65,20 @@ def main():
             prediction = modelPoly.predict(df_prepared)
         elif select_model == "RBF-SVC":
             prediction = modelRBF.predict(df_prepared)
+        elif select_model == "AdaBoost":
+            prediction = modelAda.predict(df_prepared)
+        elif select_model == "Bagging":
+            prediction = modelBag.predict(df_prepared)
+        elif select_model == "GradientBoost":
+            prediction = modelGradBoost.predict(df_prepared)
+        elif select_model == "DecisionTree":
+            prediction = modelDT.predict(df_prepared)
+        elif select_model == "RandomForest":
+            prediction = modelRF.predict(df_prepared)
+        elif select_model == "Voting":
+            prediction = modelVote.predict(df_prepared)
+        elif select_model == "XGBoost":
+            prediction = modelXGB.predict(df_prepared)
         if prediction == 0:
             output = 'No Stroke'
         elif prediction == 1:
